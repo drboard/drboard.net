c3b43a932bc0cf0dbac5071db21f6de3c6536620
Dustin Boardman <drboard@gmail.com>
2016-07-13 09:47:04 -0500

    stub out directory structure and build gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp    = require('gulp');
+var scss    = require('gulp-sass');
+var uglify  = require('gulp-uglify');
+var browserSync = require('browser-sync');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('build', function() {
+ 
+  // copy images to build directory
+  gulp.src('src/images/*')
+    .pipe(gulp.dest('build/images'));
+
+  // minify js
+  gulp.src('src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+ 
+  // minify scss -> css
+  gulp.src('src/scss/**/*.scss') 
+    .pipe(scss())
+    .pipe(gulp.dest('build/js'));
+
+  // minify html
+  gulp.src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true, minifyCSS: true, minifyJS: true }))
+    .pipe(gulp.dest('build'));
+
+  return;
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'src'
+    }
+  });
+});
+
+gulp.task('serve', ['browserSync', 'scss'], function() {
+  gulp.watch('src/scss/**/*.scss', ['scss']);
+  gulp.watch('src/*.html', browserSync.reload);
+  gulp.watch('src/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('scss', function() {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(scss())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
